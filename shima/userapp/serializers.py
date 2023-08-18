8be6d286fc63494049267da7373734d8a19249fd
Nihal <nihalroshan55@gmail.com>
2023-08-18 12:44:02 +0530

    serializer update

--- shima/userapp/serializers.py
+++ shima/userapp/serializers.py
@@ -7,29 +7,7 @@ from userapp.models import Users,leave_application,Attendance
 class UserSerializer(ModelSerializer):
     class Meta:
         model = Users
-        fields = [
-            'id',
-            'name',
-            'profile_picture',
-            'date_of_birth',
-            'email',
-            'gender',
-            'password',
-            'guardian_name',
-            'guardian_contact_number',
-            'number',
-            'married',
-            'alternate_number',
-            'is_form_filled',
-            'last_payment',
-            "is_superuser",
-            'address',
-            'pincode',
-            'post',
-            'aadhar_number',
-            'age',
-            # "_all_",
-        ]
+        fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
         
 
