9b66bb84c86df5c5e1fa6829336964653406544d
Nihal <nihalroshan55@gmail.com>
2023-09-01 20:33:11 +0530

    url included

--- shima/socketSystem/urls.py
+++ shima/socketSystem/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from socketSystem.views import NotificationViewSet
+from socketSystem.views import NotificationViewSet,Notification
 
 router = DefaultRouter()
 router.register(r'notifications', NotificationViewSet)
+router.register(r'notification/get', Notification)
+
 
 urlpatterns = [
     path('', include(router.urls)),
